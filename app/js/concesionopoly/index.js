2a2eabd4f4922d4045018e9461efe5c3db0086c4
Matías Lescano <matiasj.lescano@gmail.com>
2015-06-28 23:36:53 -0300

    chip positions

--- app/js/concesionopoly/index.js
+++ app/js/concesionopoly/index.js
@@ -2,6 +2,5 @@ import Browser from './browser'
 
 export default function Concesionopoly (board) {
   let browser = new Browser(board)
-  let engine = browser.engine
-  return engine.getState.bind(engine.getState)
+  return browser
 }
